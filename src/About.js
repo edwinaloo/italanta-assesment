b131779bfc1457a038658a9c48fc363b1dc1b298
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-11-03 17:34:23 +0300

    added stlying to about.js component

--- src/About.js
+++ src/About.js
@@ -9,23 +9,110 @@ const About = () => {
       </header>
 
       <div className="about-section">
-        {/* add background image here */}
         <h2>HOW TO RECOGNIZE AN ELEWA MEMBER</h2>
         <p>
-          Dependeble, Creative, Supportive, OpenMinded and Fun*
-
+          Dependable, Creative, Supportive, Open-Minded, and Fun*<br />
           *SERIOUS WHEN IT MATTERS
         </p>
       </div>
 
       <div className="about-section">
-        <h2>Our Team</h2>
+        <h2>We care</h2>
         <p>
-          Meet our dedicated team of professionals who work tirelessly to achieve our mission.
+          Elewa is a mission-driven organization. We make use of our cooperative and shared culture to drive the needle for the development of people and the environment. We care about our growth, but also care deeply about the context surrounding us.
+        </p>
+        <p>
+          Our investments are, therefore, not limited to internal ones but contribute heavily to our community and environment. From training the next scout leaders on sustainable practices to bridging the employment gap for junior software developers.
         </p>
       </div>
 
-      {/* Add more content sections as needed */}
+      <div className="about-section">
+        <h2>Holistic solutions</h2>
+        <h3>
+          We go beyond a simple patch-up but develop lasting solutions through holistic design.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h2>Impact</h2>
+        <h3>
+          Impact as a direct or indirect result. All our respective organizations have underlying theories of change.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h2>Open data</h2>
+        <h3>
+          Sharing is caring. We share what we learn. As proof, we've open-sourced all our internal projects.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h1>Team | Management</h1>
+        <div>
+          {/* Add images */}
+        </div>
+      </div>
+
+      <div className="about-section">
+        <h1>Our culture & Values</h1>
+        <h3>
+          Our culture, CODE-(T), defines us. It protects and guides us to success. It keeps our team and management accountable to itself, its objective, and our long-term mission.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h1>Cooperative</h1>
+        <h3>
+          One for all, all for one! We have a cooperative and open mindset. If one of us grows, all will follow.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h1>Ownership</h1>
+        <h3>
+          We are dependable and honor our commitments. When things don't go as planned, as they sometimes do, we communicate early to avoid catastrophe.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h1>Detail</h1>
+        <h3>
+          We are a "Centre of Excellence" as per global standards. We only compete with ourselves, but find in the global standard our only worthy rival.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h1>Empathy</h1>
+        <h3>
+          We are nothing without people, and people have different contexts and needs. Our organization acts as a safe haven and shield for those in need.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h1>Transparency</h1>
+        <h3>
+          Transparency, an open mind, and an open heart hold our organization together. We commit to and promote full transparency to ourselves, our stakeholders, and our beneficiaries. We furthermore invest heavily in open-source and open-data projects, as true potential lies everywhere. Not just within.
+        </h3>
+      </div>
+
+      <div className="about-section">
+        <h2>How to get there</h2>
+        <h3>
+          Based in the heart of Westlands, Nairobi, Kenya, we are easily accessible from all parts of the city. Not in town? Teams, Meets, or Zoom are our second name. Book an appointment today via contact@elewa.ke!
+        </h3>
+        {/* Add Google Map directions */}
+        {/* Add a button */}
+      </div>
+
+      <div className="about-section">
+        <h1>Our history</h1>
+        {/* Add complex UI */}
+      </div>
+
+      <div className="about-section">
+        <h2>Learn more about <b><h1><strong>Elewa's social impact</strong></h1></b></h2>
+      </div>
     </div>
   );
 };
