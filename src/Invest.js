cfaedfbc059e8d4b0c83b62aec45eb2f057ea79e
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-11-03 18:29:19 +0300

    added the styling for invest.css

--- src/Invest.js
+++ src/Invest.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './invest.css'; // Make sure to create a corresponding CSS file
 
 const Invest = () => {
   return (
@@ -17,51 +18,46 @@ const Invest = () => {
       <div className="invest-section">
         <h2>How we see it differently</h2>
         <p>
-        The global South is rapidly catching up with the "developed" world. It's time for you to get to know the rapid development, innovation and drive going on in this part of the world (East-Africa, South Asia and beyond).
+          The global South is rapidly catching up with the "developed" world. It's time for you to get to know the rapid development, innovation, and drive going on in this part of the world (East-Africa, South Asia, and beyond).
         </p>
       </div>
 
       <div className="invest-section">
         <h2>Key figures</h2>
         <p>
-          {/* add more info. */}
+          {/* Add more info. */}
         </p>
       </div>
 
-      <div>
+      <div className="invest-section">
         <h1>Elewa NV, a multi-stakeholder cooperation</h1>
         <h3>
-        At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company which controls the assets of all Elewa activities.
+          At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company that controls the assets of all Elewa activities.
 
-Elewa NV is owned and controlled by Elewa's founder, a small community of investors which share in the vision of Elewa and Elewa employees based throughout the activities of the group.
+          Elewa NV is owned and controlled by Elewa's founder, a small community of investors who share the vision of Elewa, and Elewa employees based throughout the activities of the group.
         </h3>
       </div>
 
-      <div>
+      <div className="invest-section">
         <h1>The Elewa Creative Hub</h1>
         <h3>
-        Coming soon; The Elewa Creative Hub Lies at the heart of our organization. A two-acre property at the center of Nairobi's creative district, the hub connects all Elewa's Activities under a single banner.
+          Coming soon; The Elewa Creative Hub lies at the heart of our organization. A two-acre property at the center of Nairobi's creative district, the hub connects all Elewa's activities under a single banner.
 
-Through partnerships with creative community, the Elewa Hub will organize vibrant activites that bring talents from different fields (tech, business, art, fashion) together to nurture continuous innovation.
+          Through partnerships with the creative community, the Elewa Hub will organize vibrant activities that bring talents from different fields (tech, business, art, fashion) together to nurture continuous innovation.
         </h3>
       </div>
 
-      <div>
+      <div className="invest-section">
         <h1>Investing in Elewa NV</h1>
         <h3>
-        From time to time, we offer investment opportunities (private equity & debt) to out community of investors. Interested to take part when this opportunity arrives?
+          From time to time, we offer investment opportunities (private equity & debt) to our community of investors. Interested in taking part when this opportunity arrives?
         </h3>
-        {/* add button */}
+        <button className="invest-button">Learn More</button>
       </div>
 
-      <div>
-        <h1>
-        Become part of our mission.
-And start investing
-        </h1>
+      <div className="invest-section">
+        <h1>Become part of our mission. And start investing.</h1>
       </div>
-
-      {/* Add more sections and content as needed */}
     </div>
   );
 };
