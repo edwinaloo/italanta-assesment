dd54ff1a6588dbe3a51e3a5fb532b7fd53b97cd1
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-11-03 18:17:00 +0300

    added data info into invest.js

--- src/Invest.js
+++ src/Invest.js
@@ -8,24 +8,57 @@ const Invest = () => {
       </header>
 
       <div className="invest-section">
-        <h2>Why Invest with Us</h2>
+        <h5>Trade, not aid</h5>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+          Southern innovations addressing global problems
         </p>
       </div>
 
       <div className="invest-section">
-        <h2>Our Investment Philosophy</h2>
+        <h2>How we see it differently</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+        The global South is rapidly catching up with the "developed" world. It's time for you to get to know the rapid development, innovation and drive going on in this part of the world (East-Africa, South Asia and beyond).
         </p>
       </div>
 
       <div className="invest-section">
-        <h2>Investment Opportunities</h2>
+        <h2>Key figures</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+          {/* add more info. */}
         </p>
+      </div>
+
+      <div>
+        <h1>Elewa NV, a multi-stakeholder cooperation</h1>
+        <h3>
+        At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company which controls the assets of all Elewa activities.
+
+Elewa NV is owned and controlled by Elewa's founder, a small community of investors which share in the vision of Elewa and Elewa employees based throughout the activities of the group.
+        </h3>
+      </div>
+
+      <div>
+        <h1>The Elewa Creative Hub</h1>
+        <h3>
+        Coming soon; The Elewa Creative Hub Lies at the heart of our organization. A two-acre property at the center of Nairobi's creative district, the hub connects all Elewa's Activities under a single banner.
+
+Through partnerships with creative community, the Elewa Hub will organize vibrant activites that bring talents from different fields (tech, business, art, fashion) together to nurture continuous innovation.
+        </h3>
+      </div>
+
+      <div>
+        <h1>Investing in Elewa NV</h1>
+        <h3>
+        From time to time, we offer investment opportunities (private equity & debt) to out community of investors. Interested to take part when this opportunity arrives?
+        </h3>
+        {/* add button */}
+      </div>
+
+      <div>
+        <h1>
+        Become part of our mission.
+And start investing
+        </h1>
       </div>
 
       {/* Add more sections and content as needed */}
