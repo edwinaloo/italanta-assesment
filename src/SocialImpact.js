82503892832dc51fba9c7bc4244dcda55607c5e2
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-11-03 04:50:09 +0300

    added a cooperative mindset

--- src/SocialImpact.js
+++ src/SocialImpact.js
@@ -3,30 +3,26 @@ import './SocialImpact.css';
 
 const SocialImpact = () => {
   return (
-    <div className="social-impact-content">
-      <h2><a href="https://elewa.ke">elewa group</a></h2>
+    <div className='social-impact-content'>
+            <h2><a href="https://elewa.ke">elewa group</a></h2>
       <h1>Social Impact</h1>
       <p>
         <h5>Scaling impact. Beyond the norm</h5>
         <h1><b>Human and Environmental Impact through Social Enterprise</b></h1>
       </p>
 
-        <h2>
-          733+733k200+ 56
-
-          People   Careers Projects Investors
-          trained launches finished
-        </h2>
+      <img src="https://example.com/image1.jpg" alt="Image 1" />
       
-      <img src="pics/" alt="Description of the image" />
 
       <h3>
-       <div className="SocialImpact" >
-       With a young dynamic population that is eager to contribute to the global economy, East Africa has a lot to offer to the world. Now is the time to nurture sustainable, inclusive, and scalable businesses. The rest will take care of itself.
-
-        </div> 
-      
+        With a young dynamic population that is eager to contribute to the global economy, East Africa has a lot to offer to the world. Now is the time to nurture sustainable, inclusive, and scalable businesses. The rest will take care of itself.
       </h3>
+      <p>
+        <h1>A cooperative mindset</h1>
+        <h3>
+          Elewa has one shared objective; To unlock the true potential of individuals, teams, and the community. We believe strongly in the power of sharing ideas and continuously strive to grow each other and ourselves. Internally, but also within the larger communities in which we are active.
+        </h3>
+      </p>
       
     </div>
   );
