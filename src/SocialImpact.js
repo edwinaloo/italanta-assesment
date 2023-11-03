a80fd6de44e5df311ecf52825babdffeb448e33f
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-11-03 11:22:56 +0300

    removed unwanted pictures

--- src/SocialImpact.js
+++ src/SocialImpact.js
@@ -3,16 +3,15 @@ import './SocialImpact.css';
 
 const SocialImpact = () => {
   return (
-    <div className='social-impact-content'>
-            <h2><a href="https://elewa.ke">elewa group</a></h2>
+    <div className="social-impact-content">
+      <h2><a href="https://elewa.ke">elewa group</a></h2>
       <h1>Social Impact</h1>
       <p>
         <h5>Scaling impact. Beyond the norm</h5>
         <h1><b>Human and Environmental Impact through Social Enterprise</b></h1>
       </p>
 
-      <img src="https://example.com/image1.jpg" alt="Image 1" />
-      
+      <img src="pics/lady-impact_rnc9jy.jpg" alt="Image 1" />
 
       <h3>
         With a young dynamic population that is eager to contribute to the global economy, East Africa has a lot to offer to the world. Now is the time to nurture sustainable, inclusive, and scalable businesses. The rest will take care of itself.
@@ -23,7 +22,26 @@ const SocialImpact = () => {
           Elewa has one shared objective; To unlock the true potential of individuals, teams, and the community. We believe strongly in the power of sharing ideas and continuously strive to grow each other and ourselves. Internally, but also within the larger communities in which we are active.
         </h3>
       </p>
-      
+      <p>
+        <h1>Beyond business</h1>
+        <h3>
+          We channel a good amount of our resources into giving back.
+
+          Through projects such as our coding school in Kakuma refugee camp, training teachers in pastoral Samburu, and our open-source coding initiatives providing entry-level opportunities to hundreds of junior engineers.
+
+          These projects don't necessarily have an immediate financial return, but their impact can be tangibly measured in the long-term.
+        </h3>
+      </p>
+      <p>
+        <h1>
+          Open Knowledge
+        </h1>
+        <h3>
+          What we learn, we share. Through community events, open knowledge repositories, regular teaching moments (everyone a teacher) and academic partnerships.
+
+          We build for today, with a lens on tomorrow.
+        </h3>
+      </p>
     </div>
   );
 };
